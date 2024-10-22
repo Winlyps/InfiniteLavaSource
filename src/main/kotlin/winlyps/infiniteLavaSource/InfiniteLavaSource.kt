f0c1f1cbaf851874cea0cf0538a5202052b81908
Winlypse <nts3@o2.pl>
2024-10-22 18:13:54 +0200

    Changes!

--- src/main/kotlin/winlyps/infiniteLavaSource/InfiniteLavaSource.kt
+++ src/main/kotlin/winlyps/infiniteLavaSource/InfiniteLavaSource.kt
@@ -6,9 +6,14 @@ class InfiniteLavaSource : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("lavaSourceConversion", "true")
+        }
+        logger.info("InfiniteLavaSource plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("InfiniteLavaSource plugin has been disabled.")
     }
 }
