ef3bb656eb73dccf6e32286356bc88be6765e4f5
Winlypse <nts3@o2.pl>
2024-10-22 18:13:33 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/infiniteLavaSource/InfiniteLavaSource.kt
@@ -0,0 +1,14 @@
+package winlyps.infiniteLavaSource
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class InfiniteLavaSource : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
